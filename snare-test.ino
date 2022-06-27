b98fa49ab8402a00bf3057e55dea5eec29960da0
dlawle <39495124+dlawle@users.noreply.github.com>
2022-06-27 11:41:05 -0400

    Update snare-test.ino

--- snare-test.ino
+++ snare-test.ino
@@ -11,30 +11,42 @@ Bounce button0 = Bounce(31,15);
 #define pot1            A10 // EnvDecay
 #define pot2            A11 // Waveform 1 Freq
 
+
 // GUItool: begin automatically generated code
-AudioSynthWaveform       snareSquare;      //xy=127,180
-AudioSynthWaveform       snareTri;      //xy=128,231
-AudioSynthNoiseWhite     snareNoise;         //xy=128,275
-AudioMixer4              snareMix;         //xy=280,221
-AudioSynthWaveformSine   snareContour;          //xy=287,130
-AudioAmplifier           snareAmp;           //xy=410,70
-AudioFilterStateVariable snareLP;        //xy=427,224
-AudioEffectMultiply      snareVCA;      //xy=593,223
-AudioEffectEnvelope      snareEnv;      //xy=763,221
-AudioOutputI2S           i2s1;           //xy=914,220
-AudioConnection          patchCord1(snareSquare, 0, snareMix, 1);
-AudioConnection          patchCord2(snareTri, 0, snareMix, 2);
-AudioConnection          patchCord3(snareNoise, 0, snareMix, 3);
-AudioConnection          patchCord4(snareMix, 0, snareLP, 0);
-AudioConnection          patchCord5(snareContour, snareAmp);
-AudioConnection          patchCord6(snareAmp, 0, snareLP, 1);
-AudioConnection          patchCord7(snareAmp, 0, snareVCA, 0);
-AudioConnection          patchCord8(snareLP, 2, snareVCA, 1);
-AudioConnection          patchCord9(snareVCA, snareEnv);
-AudioConnection          patchCord10(snareEnv, 0, i2s1, 0);
-AudioConnection          patchCord11(snareEnv, 0, i2s1, 1);
-AudioControlSGTL5000     sgtl5000_1;     //xy=132,73
+AudioSynthWaveformSineModulated snareFM1;       //xy=194,807
+AudioSynthNoiseWhite     snareNoise;     //xy=223,959
+AudioSynthWaveformSineModulated snareFM2;       //xy=327,869
+AudioEffectEnvelope      snareNoiseEnv;  //xy=452,948
+AudioEffectEnvelope      snareFMEnv;     //xy=478,886
+AudioSynthWaveformSineModulated snareClickCarrier; //xy=534,795
+AudioEffectEnvelope      snareClickEnv;  //xy=567,749
+AudioFilterStateVariable snareNoiseHPF;  //xy=636,949
+AudioSynthWaveformSineModulated snareBody;      //xy=639,885
+AudioFilterStateVariable snareClickFilter; //xy=749,772
+AudioMixer4              snareMixer;     //xy=835,894
+AudioEffectEnvelope      snareAmpEnv;    //xy=1009,895
+AudioAnalyzePeak         snarePeak;      //xy=1065,845
+AudioFilterStateVariable finalSnareFilter; //xy=1262,875
+AudioOutputI2S           i2s1;           //xy=1523,873
+AudioConnection          patchCord1(snareFM1, snareFM2);
+AudioConnection          patchCord2(snareNoise, snareNoiseEnv);
+AudioConnection          patchCord3(snareFM2, snareFMEnv);
+AudioConnection          patchCord4(snareNoiseEnv, 0, snareNoiseHPF, 0);
+AudioConnection          patchCord5(snareFMEnv, snareBody);
+AudioConnection          patchCord6(snareClickCarrier, snareClickEnv);
+AudioConnection          patchCord7(snareClickEnv, snareClickCarrier);
+AudioConnection          patchCord8(snareClickEnv, 0, snareClickFilter, 0);
+AudioConnection          patchCord9(snareNoiseHPF, 2, snareMixer, 2);
+AudioConnection          patchCord10(snareBody, 0, snareMixer, 1);
+AudioConnection          patchCord11(snareClickFilter, 1, snareMixer, 0);
+AudioConnection          patchCord12(snareMixer, snareAmpEnv);
+AudioConnection          patchCord13(snareAmpEnv, snarePeak);
+AudioConnection          patchCord14(snareAmpEnv, 0, finalSnareFilter, 0);
+AudioConnection          patchCord15(finalSnareFilter, 2, i2s1, 0);
+AudioConnection          patchCord16(finalSnareFilter, 2, i2s1, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=306,129
 // GUItool: end automatically generated code
+
 
 void setup() {
   // Main setup
@@ -46,34 +58,44 @@ void setup() {
   // Pins
   pinMode(31, INPUT_PULLUP);
 
-  // Snare 
-  snareSquare.begin(WAVEFORM_SQUARE);
-  snareSquare.frequency(50);
-  snareSquare.amplitude(1.5);
+  snareAmpEnv.attack(1);
+  snareAmpEnv.sustain(0);
+  snareAmpEnv.decay(100);
+  snareAmpEnv.release(1);
 
-  snareTri.begin(WAVEFORM_TRIANGLE);
-  snareTri.frequency(50);
-  snareTri.amplitude(1.5);
+  snareNoiseEnv.delay(10);
+  snareNoiseEnv.attack(150);
+  snareNoiseEnv.sustain(1);
+  snareNoiseEnv.release(1);
 
+  snareBody.frequency(50);
   snareNoise.amplitude(1);
 
-  snareContour.frequency(50);
-  snareContour.amplitude(0.5);
-  snareAmp.gain(0.5);
+  snareNoiseHPF.frequency(1000);;
 
-  snareLP.resonance(1);
-  snareLP.frequency(1500);
+  snareClickCarrier.frequency(1500);
+  snareClickEnv.attack(0);
+  snareClickEnv.sustain(0);
+  snareClickEnv.decay(500);
+  snareClickEnv.release(1);
+  snareClickFilter.frequency(1500);
+  snareClickFilter.resonance(5);
 
-  snareMix.gain(0, 0.5);
-  snareMix.gain(1, 0.5);
-  snareMix.gain(2, 0.5);
-  snareMix.gain(3, 0.5);
+  snareFM1.frequency(1500);
+  snareFM2.frequency(400);
 
-  snareEnv.attack(0);
-  snareEnv.hold(5);
-  snareEnv.delay(0);
-  snareEnv.sustain(0);
-  snareEnv.release(70);
+  snareFMEnv.attack(0);
+  snareFMEnv.sustain(0);
+  snareFMEnv.decay(50);
+  snareFMEnv.release(1);
+
+  finalSnareFilter.frequency(100);
+  finalSnareFilter.resonance(1);
+  finalSnareFilter.octaveControl(7);
+
+  snareMixer.gain(0,0.5);
+  snareMixer.gain(1,0.5);
+  snareMixer.gain(2,0.3);
 
 }
 
@@ -83,14 +105,21 @@ void loop() {
   float knob1 = (float)analogRead(pot1)/2;   // EnvDecay
   float knob2 = (float)analogRead(pot2)/4;   // Waveform 1 Freq
   
-  snareEnv.decay(knob1);
-  snareTri.frequency(knob2);
-  snareSquare.frequency(knob2);
-  snareContour.frequency(knob2);
+  snareAmpEnv.decay(knob1);
+  snareFM1.frequency(knob2);
+  snareFM2.frequency(knob2);
+  snareBody.frequency(knob2);
+  snareClickCarrier.frequency(knob2);
   
   button0.update();
   if (button0.fallingEdge()) {
-    snareEnv.noteOn();
+      snareBody.phase(0);
+      snareFM1.phase(180);
+      snareFM2.phase(180);
+      snareFMEnv.noteOn();
+      snareClickEnv.noteOn();
+      snareNoiseEnv.noteOn();
+      snareAmpEnv.noteOn();
     timeout = 0;
   }
   
