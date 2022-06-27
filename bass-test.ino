4165bcb9475ef414a5f09c46ddcee915580302c1
dlawle <39495124+dlawle@users.noreply.github.com>
2022-06-27 09:26:53 -0400

    Update bass-test.ino

--- bass-test.ino
+++ bass-test.ino
@@ -11,17 +11,17 @@
 #include <SerialFlash.h>
 
 // GUItool: begin automatically generated code
-AudioSynthWaveform       waveform1;      //xy=123,317
-AudioEffectEnvelope      envelope1;      //xy=328,339
-AudioSynthWaveformModulated waveformMod1;   //xy=502,286
-AudioFilterStateVariable filter1;        //xy=666,315
+AudioSynthWaveform       kickPulse;      //xy=123,317
+AudioEffectEnvelope      kickPulseEnv;      //xy=328,339
+AudioSynthWaveformModulated kickSine;   //xy=502,286
+AudioFilterStateVariable kickLPF;        //xy=666,315
 AudioOutputI2S           i2s1;           //xy=838,326
-AudioConnection          patchCord1(waveform1, envelope1);
-AudioConnection          patchCord2(envelope1, 0, waveformMod1, 0);
-AudioConnection          patchCord3(envelope1, 0, filter1, 1);
-AudioConnection          patchCord4(waveformMod1, 0, filter1, 0);
-AudioConnection          patchCord5(filter1, 2, i2s1, 0);
-AudioConnection          patchCord6(filter1, 2, i2s1, 1);
+AudioConnection          patchCord1(kickPulse, kickPulseEnv);
+AudioConnection          patchCord2(kickPulseEnv, 0, kickSine, 0);
+AudioConnection          patchCord3(kickPulseEnv, 0, kickLPF, 1);
+AudioConnection          patchCord4(kickSine, 0, kickLPF, 0);
+AudioConnection          patchCord5(kickLPF, 2, i2s1, 0);
+AudioConnection          patchCord6(kickLPF, 2, i2s1, 1);
 AudioControlSGTL5000     sgtl5000_1;     //xy=100,46
 // GUItool: end automatically generated code
 
@@ -34,7 +34,20 @@ void setup() {
   sgtl5000_1.volume(0.9);
   AudioMemory(48);
   
-  //kick////////////////////////////////////
+  //kick 
+  kickPulse.begin(WAVEFORM_PULSE);
+  kickPulse.frequency(10);
+
+  kickPulseEnv.attack(1);
+  kickPulseEnv.sustain(0);
+  kickPulseEnv.decay(150);
+  kickPulseEnv.release(1);
+
+  kickSine.begin(WAVEFORM_SINE)
+  kickSine.frequency(10);
+
+  kickLPF.frequency(1500);
+  kickLPF.resonance(5);
 
   pinMode(31,INPUT_PULLUP);
 }
@@ -44,16 +57,13 @@ void loop() {
   
     gateIn.update();
     if (gateIn.fallingEdge()) {
-      kickClickEnv.noteOn();
-      kickSwoopEnv.noteOn();
-      kickAmpEnv.noteOn();
+      kickPulseEnv.noteOn();
       Serial.println("Button Released, sending NoteOn");
+      Serial.println();
     }
     
     if (gateIn.risingEdge()) {
-      kickClickEnv.noteOff();
-      kickSwoopEnv.noteOff();
-      kickAmpEnv.noteOff();
+      kickPulseEnv.noteOff();
       Serial.println("Button Released, sending NoteOff");
       Serial.println();
     }
