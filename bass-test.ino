34920865b9d33e576d93ee436a9798b45a1ca9b8
dlawle <39495124+dlawle@users.noreply.github.com>
2022-06-27 10:04:21 -0400

    Update bass-test.ino

--- bass-test.ino
+++ bass-test.ino
@@ -3,69 +3,99 @@
 #include <SPI.h>
 #include <SD.h>
 #include <SerialFlash.h>
+#include <Bounce.h> 
 
-#include <Audio.h>
-#include <Wire.h>
-#include <SPI.h>
-#include <SD.h>
-#include <SerialFlash.h>
+Bounce button0 = Bounce(31,15);
 
 // GUItool: begin automatically generated code
-AudioSynthWaveform       kickPulse;      //xy=123,317
-AudioEffectEnvelope      kickPulseEnv;      //xy=328,339
-AudioSynthWaveformModulated kickSine;   //xy=502,286
-AudioFilterStateVariable kickLPF;        //xy=666,315
-AudioOutputI2S           i2s1;           //xy=838,326
-AudioConnection          patchCord1(kickPulse, kickPulseEnv);
-AudioConnection          patchCord2(kickPulseEnv, 0, kickSine, 0);
-AudioConnection          patchCord3(kickPulseEnv, 0, kickLPF, 1);
-AudioConnection          patchCord4(kickSine, 0, kickLPF, 0);
-AudioConnection          patchCord5(kickLPF, 2, i2s1, 0);
-AudioConnection          patchCord6(kickLPF, 2, i2s1, 1);
-AudioControlSGTL5000     sgtl5000_1;     //xy=100,46
+AudioSynthWaveform       waveform2;      //xy=168,195
+AudioSynthWaveform       waveform1;      //xy=178,396
+AudioFilterStateVariable filter1;        //xy=356,131
+AudioFilterStateVariable filter2;        //xy=356,323
+AudioEffectEnvelope      envelope1;      //xy=561,505
+AudioMixer4              mixer1;         //xy=570,216
+AudioEffectMultiply      multiply1;      //xy=829,310
+AudioOutputI2S           i2s2;           //xy=1035,311
+AudioConnection          patchCord1(waveform2, 0, filter1, 0);
+AudioConnection          patchCord2(waveform1, envelope1);
+AudioConnection          patchCord3(waveform1, 0, filter2, 0);
+AudioConnection          patchCord4(filter1, 1, mixer1, 1);
+AudioConnection          patchCord5(filter2, 2, mixer1, 2);
+AudioConnection          patchCord6(envelope1, 0, multiply1, 1);
+AudioConnection          patchCord7(mixer1, 0, multiply1, 0);
+AudioConnection          patchCord8(multiply1, 0, i2s2, 0);
+AudioConnection          patchCord9(multiply1, 0, i2s2, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=208,89
 // GUItool: end automatically generated code
 
-#include <Bounce.h>
-Bounce gateIn = Bounce(31,15);
+
+// inputs
+#define pot1            A10 // EnvDecay
+#define pot2            A11 // Waveform 1 Freq
 
 void setup() {
+  // Main setup
   Serial.begin(9600);
+  AudioMemory(20);
   sgtl5000_1.enable();
   sgtl5000_1.volume(0.9);
-  AudioMemory(48);
   
-  //kick 
-  kickPulse.begin(WAVEFORM_PULSE);
-  kickPulse.frequency(10);
+  // Pins
+  pinMode(31, INPUT_PULLUP);
+  
+  // Mixer 
+  mixer1.gain(0, 0.5);
+  mixer1.gain(1, 0.5);
+  mixer1.gain(2, 0.5);
+  mixer1.gain(3 , 0.5);
+  
+  // Waveform 1
+  waveform1.begin(WAVEFORM_SINE);
+  waveform1.amplitude(1.5);
+  waveform1.frequency(50);
+  //waveform1.pulseWidth(.5);
 
-  kickPulseEnv.attack(1);
-  kickPulseEnv.sustain(0);
-  kickPulseEnv.decay(150);
-  kickPulseEnv.release(1);
+  // Waveform 2
+  waveform2.begin(WAVEFORM_SINE);
+  waveform2.amplitude(1.5);
+  waveform2.frequency(50);
+  //waveform2.pulseWidth(.5);
+  
+  //envelope
+  envelope1.attack(0);
+  envelope1.hold(5);
+  envelope1.delay(0);
+  envelope1.sustain(0);
+  envelope1.release(70);
 
-  kickSine.begin(WAVEFORM_SINE)
-  kickSine.frequency(10);
-
-  kickLPF.frequency(1500);
-  kickLPF.resonance(5);
-
-  pinMode(31,INPUT_PULLUP);
+  // Filter Resonance
+  filter1.resonance(1);
+  filter2.resonance(1);
+  filter1.frequency(300);
+  filter1.frequency(300);
 }
 
-void loop() {
-  AudioNoInterrupts();
+elapsedMillis timeout = 0;
+
+void loop() { 
+  float knob1 = (float)analogRead(pot1)/2;   // EnvDecay
+  float knob2 = (float)analogRead(pot2)/4;   // Waveform 1 Freq
+  //AudioNoInterrupts();
   
-    gateIn.update();
-    if (gateIn.fallingEdge()) {
-      kickPulseEnv.noteOn();
-      Serial.println("Button Released, sending NoteOn");
-      Serial.println();
+  envelope1.decay(knob1);
+  waveform1.frequency(knob2);
+  waveform2.frequency(knob2);
+
+    button0.update();
+    if (button0.fallingEdge()) {
+      envelope1.noteOn();
+      timeout = 0;
     }
     
-    if (gateIn.risingEdge()) {
-      kickPulseEnv.noteOff();
+    if (button0.risingEdge()) {
+      //envelope1.noteOff();
       Serial.println("Button Released, sending NoteOff");
       Serial.println();
+      timeout = 0;
     }
-  
 }
