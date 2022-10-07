5207954aeb987beb452df59c2d4a8bb759a59d74
dlawle <daniel.lawler@outlook.com>
2022-10-07 16:13:35 -0400

    Update 4drum.ino
    Adding possible "hold to mod" function outlined in otemrellik's TeensyDrum.
    
    Also moving to 3 drums and one hat

--- 4drum/4drum.ino
+++ 4drum/4drum.ino
@@ -12,8 +12,6 @@ Bounce gate1 = Bounce(24,5);
 Bounce gate2 = Bounce(25,5);
 Bounce gate3 = Bounce(26,5);
 Bounce gate4 = Bounce(27,5);
-Bounce gate4 = Bounce(33,5);
-
 
 // inputs
 #define pot1            A17 // frequency
@@ -21,69 +19,43 @@ Bounce gate4 = Bounce(33,5);
 #define pot3            A15 // secondMix
 #define pot4            A14 // pitchMod
 
-#include <Audio.h>
-#include <Wire.h>
-#include <SPI.h>
-#include <SD.h>
-#include <SerialFlash.h>
-
 // GUItool: begin automatically generated code
-AudioSynthWaveformSineModulated HHSineMod3; //xy=173,922
-AudioSynthWaveformSineModulated HHSineMod1;     //xy=198,682
-AudioSynthWaveformSineModulated HHSineMod4; //xy=197,974
-AudioSynthWaveformSineModulated HHSineMod2;     //xy=222,734
-AudioEffectEnvelope      HHModEnv1; //xy=353,973
-AudioEffectEnvelope      HHModEnv;       //xy=376,732
-AudioSynthNoiseWhite     HHNoise1; //xy=433,1084
-AudioSynthNoiseWhite     HHNoise;        //xy=451,842
-AudioSynthWaveformSineModulated HHSineCarrier1; //xy=517,974
-AudioSynthWaveformSineModulated HHSineCarrier;  //xy=535,732
-AudioEffectEnvelope      HHModAMpEnv1; //xy=566,1022
-AudioEffectEnvelope      HHnoiseEnv1; //xy=577,1084
-AudioEffectEnvelope      HHModAMpEnv;    //xy=584,780
-AudioEffectEnvelope      HHnoiseEnv;     //xy=595,842
-AudioMixer4              HHMixer1; //xy=740,1091
-AudioMixer4              HHMixer;        //xy=761,853
-AudioFilterStateVariable HHfilter1; //xy=909,1044
-AudioFilterStateVariable HHfilter;       //xy=927,802
-AudioSynthSimpleDrum     drum2;          //xy=1013,697
-AudioSynthSimpleDrum     drum1;          //xy=1045,650
-AudioEffectEnvelope      HHAmpEnv1; //xy=1054,1062
-AudioEffectEnvelope      HHAmpEnv;       //xy=1072,820
-AudioAnalyzePeak         HHPeak1; //xy=1081,1002
-AudioAnalyzePeak         HHPeak;         //xy=1099,760
-AudioMixer4              mixer1;         //xy=1288,718
-AudioOutputI2S           i2s1;           //xy=1593,667
-AudioConnection          patchCord1(HHSineMod3, HHSineMod4);
-AudioConnection          patchCord2(HHSineMod1, HHSineMod2);
-AudioConnection          patchCord3(HHSineMod4, HHModEnv1);
-AudioConnection          patchCord4(HHSineMod2, HHModEnv);
-AudioConnection          patchCord5(HHModEnv1, HHSineCarrier1);
-AudioConnection          patchCord6(HHModEnv, HHSineCarrier);
-AudioConnection          patchCord7(HHNoise1, HHnoiseEnv1);
-AudioConnection          patchCord8(HHNoise, HHnoiseEnv);
-AudioConnection          patchCord9(HHSineCarrier1, HHModAMpEnv1);
-AudioConnection          patchCord10(HHSineCarrier, HHModAMpEnv);
-AudioConnection          patchCord11(HHModAMpEnv1, 0, HHMixer1, 0);
-AudioConnection          patchCord12(HHModAMpEnv1, 0, HHfilter1, 1);
-AudioConnection          patchCord13(HHnoiseEnv1, 0, HHMixer1, 1);
-AudioConnection          patchCord14(HHModAMpEnv, 0, HHMixer, 0);
-AudioConnection          patchCord15(HHModAMpEnv, 0, HHfilter, 1);
-AudioConnection          patchCord16(HHnoiseEnv, 0, HHMixer, 1);
-AudioConnection          patchCord17(HHMixer1, 0, HHfilter1, 0);
-AudioConnection          patchCord18(HHMixer, 0, HHfilter, 0);
-AudioConnection          patchCord19(HHfilter1, 2, HHAmpEnv1, 0);
-AudioConnection          patchCord20(HHfilter, 2, HHAmpEnv, 0);
-AudioConnection          patchCord21(drum2, 0, mixer1, 1);
-AudioConnection          patchCord22(drum1, 0, mixer1, 0);
-AudioConnection          patchCord23(HHAmpEnv1, HHPeak1);
-AudioConnection          patchCord24(HHAmpEnv1, 0, mixer1, 3);
-AudioConnection          patchCord25(HHAmpEnv, HHPeak);
-AudioConnection          patchCord26(HHAmpEnv, 0, mixer1, 2);
-AudioConnection          patchCord27(mixer1, 0, i2s1, 0);
-AudioConnection          patchCord28(mixer1, 0, i2s1, 1);
-AudioControlSGTL5000     sgtl5000_1;     //xy=541,243
+AudioSynthWaveformSineModulated HHSineMod1;     //xy=355.00001525878906,299.50000190734863
+AudioSynthWaveformSineModulated HHSineMod2;     //xy=523.0000152587891,301.50000190734863
+AudioEffectEnvelope      HHModEnv;       //xy=677.0000152587891,299.50000190734863
+AudioSynthNoiseWhite     HHNoise;        //xy=752.0000152587891,409.50000190734863
+AudioSynthWaveformSineModulated HHSineCarrier;  //xy=836.0000152587891,299.50000190734863
+AudioEffectEnvelope      HHModAMpEnv;    //xy=885.0000152587891,347.50000190734863
+AudioEffectEnvelope      HHnoiseEnv;     //xy=896.0000152587891,409.50000190734863
+AudioMixer4              HHMixer;        //xy=1062.000015258789,420.50000190734863
+AudioFilterStateVariable HHfilter;       //xy=1228.000015258789,369.50000190734863
+AudioSynthSimpleDrum     drum3;          //xy=1358,184
+AudioSynthSimpleDrum     drum2;          //xy=1360,149
+AudioSynthSimpleDrum     drum1;          //xy=1361,114
+AudioEffectEnvelope      HHAmpEnv;       //xy=1373.000015258789,387.50000190734863
+AudioAnalyzePeak         HHPeak;         //xy=1400.000015258789,327.50000190734863
+AudioMixer4              mixer1;         //xy=1595,182
+AudioOutputI2S           i2s1;           //xy=1900,131
+AudioConnection          patchCord1(HHSineMod1, HHSineMod2);
+AudioConnection          patchCord2(HHSineMod2, HHModEnv);
+AudioConnection          patchCord3(HHModEnv, HHSineCarrier);
+AudioConnection          patchCord4(HHNoise, HHnoiseEnv);
+AudioConnection          patchCord5(HHSineCarrier, HHModAMpEnv);
+AudioConnection          patchCord6(HHModAMpEnv, 0, HHMixer, 0);
+AudioConnection          patchCord7(HHModAMpEnv, 0, HHfilter, 1);
+AudioConnection          patchCord8(HHnoiseEnv, 0, HHMixer, 1);
+AudioConnection          patchCord9(HHMixer, 0, HHfilter, 0);
+AudioConnection          patchCord10(HHfilter, 2, HHAmpEnv, 0);
+AudioConnection          patchCord11(drum3, 0, mixer1, 2);
+AudioConnection          patchCord12(drum2, 0, mixer1, 1);
+AudioConnection          patchCord13(drum1, 0, mixer1, 0);
+AudioConnection          patchCord14(HHAmpEnv, HHPeak);
+AudioConnection          patchCord15(HHAmpEnv, 0, mixer1, 3);
+AudioConnection          patchCord16(mixer1, 0, i2s1, 0);
+AudioConnection          patchCord17(mixer1, 0, i2s1, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=1600,33
 // GUItool: end automatically generated code
+
 
 
 // https://forum.arduino.cc/t/use-a-single-knob-for-mutli-variables/311022/14 
@@ -99,6 +71,11 @@ int led3 = 29;
 int led4 = 28;
 
 const unsigned long debounceDelay = 50;    // the debounce time milliseconds; increase if the output flickers
+
+// another trick from otem rellik's teensydrum
+bool btnDown;
+
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -138,42 +115,14 @@ void setup() {
   drum2.secondMix(0.0);
   drum2.pitchMod(1.0);
   
+  drum3.frequency(60);
+  drum3.length(800);
+  drum3.secondMix(0.0);
+  drum3.pitchMod(1.0);
+  
 
   // from https://github.com/otem/Eurorack/tree/master/TeensyDrumV2
   //HiHat 1 ////////////////////////////////////
-  HHModAMpEnv.attack(1);
-  HHModAMpEnv.decay(50);
-  HHModAMpEnv.sustain(0);
-  HHModAMpEnv.release(10);
-
-  HHSineCarrier.amplitude(0.75);  
-  HHSineCarrier.frequency(4000);
-
-  HHSineMod1.frequency(2500);
-  HHSineMod2.frequency(1250);
-
-  HHModEnv.attack(0);
-  HHModEnv.decay(50);
-  HHModEnv.sustain(0);
-  HHModEnv.release(10);
-
-  HHfilter.frequency(2000);
-  HHNoise.amplitude(0.5);
-
-  HHnoiseEnv.attack(2);
-  HHnoiseEnv.decay(250);
-  HHnoiseEnv.sustain(0);
-  HHnoiseEnv.release(10);
-
-  HHMixer.gain(0,0.3);
-  HHMixer.gain(1,0.5);
-
-  HHAmpEnv.attack(1);
-  HHAmpEnv.decay(300);
-  HHAmpEnv.sustain(0);
-  HHAmpEnv.release(10);
-
-  //HiHat 2 ////////////////////////////////////
   HHModAMpEnv.attack(1);
   HHModAMpEnv.decay(50);
   HHModAMpEnv.sustain(0);
@@ -255,38 +204,36 @@ void readButtons() {
 
       //Only change mode if the new button state is LOW (pressed)
       //depending on which mode we are in, we may jump to a different one
-      if (buttonState == LOW) {
-        switch (mode) {
-          case d1:
-            mode = d2;
-            digitalWrite(led1, LOW);
-            digitalWrite(led2, HIGH);
-            break;
-          case d2:
-            mode = d3;
-            digitalWrite(led2, LOW);
-            digitalWrite(led3, HIGH);
-            break;
-          case d3:
-            mode = d4;
-            digitalWrite(led3, LOW);
-            digitalWrite(led4, HIGH);
-            break;
-          case d4:
-            mode = d1;
-            digitalWrite(led4, LOW);
-            digitalWrite(led1, HIGH);
-            break;
-          default:
-            //something random changed mode to an invalid value? Get it back on track.
-            mode = d1;
-            digitalWrite(led2, LOW);
-            digitalWrite(led3, LOW);
-            digitalWrite(led4, LOW);
-            digitalWrite(led1, HIGH);
-            break;
+      switch (mode) {
+        case d1:
+          mode = d2;
+          digitalWrite(led1, LOW);
+          digitalWrite(led2, HIGH);
+          break;
+        case d2:
+          mode = d3;
+          digitalWrite(led2, LOW);
+          digitalWrite(led3, HIGH);
+          break;
+        case d3:
+          mode = d4;
+          digitalWrite(led3, LOW);
+          digitalWrite(led4, HIGH);
+          break;
+        case d4:
+          mode = d1;
+          digitalWrite(led4, LOW);
+          digitalWrite(led1, HIGH);
+          break;
+        default:
+          //something random changed mode to an invalid value? Get it back on track.
+          mode = d1;
+          digitalWrite(led2, LOW);
+          digitalWrite(led3, LOW);
+          digitalWrite(led4, LOW);
+          digitalWrite(led1, HIGH);
+          break;
         }
-      }
     }
   }
 
@@ -298,18 +245,22 @@ void readPots() {
   //read in the potentiometer values.
   //Depending on the mode, this will update different global variables
 
+  // this trick should allow us to only execute when the button is held down
+  btnDown = !digitalRead(modPin);
+
   float knob1 = (float)analogRead(pot1)/2;   // frequency
   float knob2 = (float)analogRead(pot2)/2;   // length
   float knob3 = (float)analogRead(pot3)/2;   // secondMix
   float knob4 = (float)analogRead(pot4)/2;   // pitchMod
 
-  if (modPin == HIGH) {
     switch (mode) {
       case d1:
-        drum1.frequency(knob1);
-        drum1.length(knob2);
-        drum1.secondMix(knob3);
-        drum1.pitchMod(knob4);
+        if(btnDown){
+          drum1.frequency(knob1);
+          drum1.length(knob2);
+          drum1.secondMix(knob3);
+          drum1.pitchMod(knob4);
+        }
         break;
       case d2:
         drum2.frequency(knob1);
@@ -318,20 +269,20 @@ void readPots() {
         drum2.pitchMod(knob4);
         break;
       case d3:
+        drum3.frequency(knob1);
+        drum3.length(knob2);
+        drum3.secondMix(knob3);
+        drum3.pitchMod(knob4);
+        break;
+      case d4:
         HHAmpEnv.decay(knob1);
         HHModEnv.decay(knob2);
         HHfilter.frequency(knob3);
         HHModAMpEnv.decay(knob4);
         break;
-      case d4:
-        HHAmpEnv1.decay(knob1);
-        HHModEnv1.decay(knob2);
-        HHfilter1.frequency(knob3);
-        HHModAMpEnv1.decay(knob4);
-        break;
     }
   }
-}
+
 
 void playDrums() {
     
@@ -349,17 +300,14 @@ void playDrums() {
     timeout = 0;
   }
   if (gate3.fallingEdge()) {
+    drum3.noteOn();
+    timeout = 0;
+  }
+  if (gate4.fallingEdge()) {
       HHnoiseEnv.noteOn();
       HHAmpEnv.noteOn();
       HHModAMpEnv.noteOn();
       HHModEnv.noteOn();
-    timeout = 0;
-  }
-  if (gate4.fallingEdge()) {
-      HHnoiseEnv1.noteOn();
-      HHAmpEnv1.noteOn();
-      HHModAMpEnv1.noteOn();
-      HHModEnv1.noteOn();
     timeout = 0;
   }
 }
